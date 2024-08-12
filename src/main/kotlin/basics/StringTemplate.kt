d82fe0b6400d4030dedf89ad8bef84e26809a016
Angel <4mr0m3r0@gmail.com>
2024-08-12 10:11:47 -0400

    more string template examples

--- src/main/kotlin/basics/StringTemplate.kt
+++ src/main/kotlin/basics/StringTemplate.kt
@@ -5,3 +5,10 @@ import kotlin.random.Random
 val random = Random(1000).nextInt()
 
 val stringTemplate = "Random number of $random"
+
+fun main() {
+    val name = readln()
+    val lastname = readln()
+    println("First name, ${if (name.isBlank()) "person" else name}!")
+    println("Lastname, ${lastname.ifBlank { "person" }}!")
+}
