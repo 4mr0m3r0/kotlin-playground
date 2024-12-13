bba35ce13c6780adc3b770b18cbf74d48573d7ba
Angel <4mr0m3r0@gmail.com>
2024-12-13 12:09:17 -0300

    Class Photo custom implementation

--- src/main/kotlin/algorithms/greedy/ClassPhotos.kt
+++ src/main/kotlin/algorithms/greedy/ClassPhotos.kt
@@ -4,10 +4,27 @@ package algorithms.greedy
  * AlgoExpert
  */
 class ClassPhotos {
-    fun classPhotos(redShirtHeights: MutableList<Int>, blueShirtHeights: MutableList<Int>): Boolean {
+    fun verification(redShirtHeights: List<Int>, blueShirtHeights: List<Int>): Boolean {
         // TODO order both arrays.
+        val redOrder = redShirtHeights.sorted()
+        val blueOrder = blueShirtHeights.sorted()
         // TODO compare red array with blue array and if all elements are lower or greater, then the class photo can be taken.
+        var differences = 0
+        var takePhoto = false
+        for ((i, redValue) in redOrder.withIndex()) {
+            if (i == 0) {
+                if (redValue > blueOrder[i]) differences = 1
+                else if (redValue < blueOrder[i]) differences = -1
+                continue
+            }
+            if (redValue > blueOrder[i] && differences == 1) takePhoto = true
+            else if (redValue < blueOrder[i] && differences == -1) takePhoto = true
+            else {
+                takePhoto = false
+                break
+            }
+        }
         // TODO return true | false depending on the result.
-        return false
+        return takePhoto
     }
 }
---
+++ src/test/kotlin/algorithms/greedy/ClassPhotosTest.kt
@@ -0,0 +1,35 @@
+package algorithms.greedy
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ClassPhotosTest {
+    private val classPhotos = ClassPhotos()
+
+    @Test
+    fun `given red less high than blue, when verification photo, then true`() {
+        val red = listOf(5, 8, 1, 2, 4)
+        val blue = listOf(6, 9, 2, 4, 5)
+
+        val result = classPhotos.verification(
+            redShirtHeights = red,
+            blueShirtHeights = blue
+        )
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun `given red same high as blue, when verification photo, then false`() {
+        val red = listOf(5, 8, 1, 2, 4)
+        val blue = listOf(9, 5, 2, 4, 5)
+
+        val result = classPhotos.verification(
+            redShirtHeights = red,
+            blueShirtHeights = blue
+        )
+
+        assertFalse(result)
+    }
+}
